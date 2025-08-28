d33ac2351945d389d6d5e1613c9f20043b7c076d
Shohan Ali <mdshohanalli578@gmail.com>
2025-08-28 20:08:39 +0600

    copy keyboard and copy count

---
+++ js/copybtn.js
@@ -0,0 +1,16 @@
+let copyButtons = document.querySelectorAll('.copy-button');
+
+for (let copyButton of copyButtons){
+    copyButton.addEventListener("click", function (){
+        let card = this.closest('.card-container');
+        let number = card.querySelector('.number').innerText;
+        alert("নাম্বার কপি হয়েছে : " + number);
+        navigator.clipboard.writeText(number);
+        let copyCount = document.querySelector('.copy-count');
+        let currentCount = parseInt(copyCount.innerText);
+        copyCount.innerText = currentCount + 1;
+    })
+
+
+
+};
