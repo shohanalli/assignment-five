347757577c5bfce0a252c7c221858f460a4393ac
Shohan Ali <mdshohanalli578@gmail.com>
2025-08-28 10:50:00 +0600

    On clicking a card's **Call Button**

--- js/style.js
+++ js/style.js
@@ -9,3 +9,30 @@ for(let hartButton of hartButtons){
     hartCounter.textContent = counter + 1;
   });
 };
+
+// On clicking a card's **Call Button**, following actions will happen
+var callButtons = document.querySelectorAll(".call-button");
+
+for(let callButton of callButtons){
+  callButton.addEventListener('click', function () {
+    let card = this.closest('.card-container');
+    let cardTittle = card.querySelector('.card-tittle');
+    let number = card.querySelector('.number');
+
+    let coinCounter =document.querySelector('.coin-counter');
+    let coinCounterConvert = parseInt(coinCounter.innerText);
+
+    if (coinCounterConvert < 20){
+
+      alert("আপনার পর্যাপ্ত কয়েন নাই, কল করতে কমপক্ষে ২০ টি কয়েন প্রয়োজন");
+    }else{
+          alert (cardTittle.innerText + " - " + number.innerText);
+       let newCoins = coinCounterConvert - 20;
+       coinCounter.innerText = newCoins;
+    }
+   
+
+  })
+}
+
+
